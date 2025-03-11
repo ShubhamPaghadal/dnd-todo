9280b844f5339d9a7babb01bcd34981a0a9c867e
ShubhamPaghadal <shubham10.tps@gmail.com>
2025-03-11 15:52:23 +0530

    CRUD

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -8,24 +8,38 @@ const Todo = () => {
   const [value, setValue] = useState("");
   const [tasks, setTasks] = useState([]);
   const [dragTask, setDragTask] = useState("");
+  const [updateItem, setUpdateItem] = useState("");
 
   const handlChange = (e) => {
     setValue(e.target.value);
   };
 
   const addItem = () => {
-    const obj = {
-      title: value,
-      status: TODO,
-      id: Date.now(),
-    };
-    setTasks((prevTask) => [...prevTask, obj]);
+    let obj;
+    if (updateItem) {
+      obj = {
+        title: value,
+        id: updateItem.id,
+        status: updateItem.status,
+      };
+      const copyTask = [...tasks];
+      const FilterList = copyTask.filter((item) => item.id !== updateItem.id);
+      setTasks((prevTask) => [...FilterList, obj]);
+      setUpdateItem("");
+    } else {
+      obj = {
+        title: value,
+        status: TODO,
+        id: Date.now(),
+      };
+      setTasks((prevTask) => [...prevTask, obj]);
+    }
     setValue("");
   };
+
   console.log("tasks....", tasks);
 
   const handleDrage = (e, task) => {
-    // console.log("task....", task);
     setDragTask(task);
   };
 
@@ -38,11 +52,11 @@ const Todo = () => {
       return item;
     });
     setTasks(copyText);
+    setDragTask("");
   };
 
   const handleOnDrop = (e) => {
     const status = e.target.getAttribute("data-status");
-    console.log("Doing.......", status);
     if (status === TODO) {
       handleDND(TODO);
     } else if (status === DOING) {
@@ -56,12 +70,23 @@ const Todo = () => {
     e.preventDefault();
   };
 
+  const DeleteTask = (item) => {
+    let copyTexts = [...tasks];
+    copyTexts = copyTexts.filter((task) => task.id !== item.id);
+    setTasks(copyTexts);
+  };
+
+  const EditTask = (task) => {
+    setUpdateItem(task);
+    setValue(task.title);
+  };
+
   return (
     <div>
       <div className="App">
         <h1>Task Manager</h1>
         <input type="text" value={value} onChange={handlChange} />
-        <button onClick={addItem}>ADD</button>
+        <button onClick={addItem}>{updateItem ? "Update" : "ADD"}</button>
         <div className="board">
           <div
             className="todo"
@@ -80,6 +105,11 @@ const Todo = () => {
                       draggable
                     >
                       {task.title}
+                      <div>
+                        <button onClick={() => EditTask(task)}>Edit</button>
+
+                        <button onClick={() => DeleteTask(task)}>Delete</button>
+                      </div>
                     </div>
                   )
               )}
@@ -91,13 +121,25 @@ const Todo = () => {
             onDragOver={OnDragOver}
             className="doing"
           >
-            <h1 className="doing-col">doing</h1>
+            <h1 className="doing-col">Doing</h1>
             {tasks.length > 0 &&
               tasks.map(
                 (task) =>
                   task.status === DOING && (
-                    <div key={task.id} draggable>
+                    <div
+                      key={task.id}
+                      onDrag={(e) => handleDrage(e, task)}
+                      draggable
+                    >
                       {task.title}
+                      <div>
+                        <span onClick={() => EditTask(task)} className="btn">
+                          Edit
+                        </span>
+                        <span onClick={() => DeleteTask(task)} className="btn">
+                          Delete
+                        </span>
+                      </div>
                     </div>
                   )
               )}
@@ -113,8 +155,20 @@ const Todo = () => {
               tasks.map(
                 (task) =>
                   task.status === DONE && (
-                    <div key={task.id} draggable>
+                    <div
+                      key={task.id}
+                      onDrag={(e) => handleDrage(e, task)}
+                      draggable
+                    >
                       {task.title}
+                      <div>
+                        <span onClick={() => EditTask(task)} className="btn">
+                          Edit
+                        </span>
+                        <span onClick={() => DeleteTask(task)} className="btn">
+                          Delete
+                        </span>
+                      </div>
                     </div>
                   )
               )}
